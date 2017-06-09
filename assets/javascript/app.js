dd72ff03624365d14ad5ef807e66e095572046fa
Marcus Hobbs <dhobbs81@gmail.com>
2017-06-09 10:29:17 -0400

    Added additional IDE generated files to the ignore file. Added intial revision of the main entry point.

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+/**
+ * @file Provides the main entry point to ExCommute
+ * @author Marcus Hobbs
+ *
+ */
+
+// Execute in strict mode as defined by the ECMAScript version 5 standard
+"use strict";
+
+// Attach the main function that will run when the DOM is ready
+// The JQuery 3.0 API recommends this usage. Other uses are deprecated.
+$(main);
+
+/**  MAIN PROCESS
+ * The main entry point to the ExCommute application.
+ * @returns true if there were no critical errors
+ */
+function main() {
+    return true;
+}
