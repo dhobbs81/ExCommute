ddf52f61f026f6ce921f0bfc5839a9ecc147eac9
Marcus Hobbs <dhobbs81@gmail.com>
2017-06-11 13:58:51 -0400

    Updated ignore file.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,11 +7,11 @@
 // Execute in strict mode as defined by the ECMAScript version 5 standard
 "use strict";
 
-// Attach the main function that will run when the DOM is ready
-// The JQuery 3.0 API recommends this usage Other uses are deprecated.
+// Attach the main function that will run when the DOM is ready The JQuery 3.0
+// API recommends this usage Other uses are deprecated.
 $(main);
 
-/**  MAIN PROCESS
+/**
  * The main entry point to the ExCommute application.
  * @returns true if there were no critical errors
  */
