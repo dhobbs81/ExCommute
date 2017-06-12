13d734f620843a4bb929471237ca154d2c33d277
Bridget Bourne <bbourne8891@hotmail.com>
2017-06-12 10:07:45 -0400

    Here is more of my materialize work with a couple javascript obstacles I wasnt able to clear yet

--- assets/javascript/display.js
+++ assets/javascript/display.js
@@ -18,36 +18,68 @@
 		 	});
 		 	var clearAll = $("#clear").click(function(){
 		 		var textClear = $("input").empty();
-		 		var sliderClear = $(".input-range").empty();
+		 		//var sliderClear = $(".input-range").empty();
+		 		var slider = document.getElementById("test5");
+		 		var sliderDefault = $("slider").val;
+		 		//var sliderCurrent = $("slider").val;
+		 		var sliderCurrent = $("slider").focus(function(){
+		 			if (sliderDefault !== sliderCurrent){
+		 			var sliderClear = $("#clear").click(function(){
+	 					sliderCurrent = 50;
+		 			});
+		 		};
+		 		
+		 		});
 		 		console.log(textClear); //Clear button appears not to be working. Work on that.
 		 		console.log(sliderClear);
 		 	});
 		 	
-		 	
+		 	var search = $("#submit").click(function(){
+		 		//insert API function here to fetch results according to filters
+		 		if(search){
+		 		var map = $("#map");
+		 		$("map").show();
+		 		$("form").hide();
+		 		$("button").hide();
+		 		$("h4").hide();
+		 		$("h5").hide();
+		 	}else{
+		 		$("#map").hide();
+		 		$(".card-panel").hide();
+
+		 	};
+		 	});
+
 		 	/*You need to be ready for the CSS to integrate w/ GoogleMaps API properly. 
 		 	Highlight selected properties, make markers highly visible and legible. 
 		 	Create spaces where additional dialogue info can be displayed, either w/ mouse click or hover.*/
-		 	var map = $("#map");
+		 	
+		 	/*!!COME BACK TO THIS FUNCTION!!       if(){}; */
 
 		 	 //var queryURL = /*insert url base here*/ + /*insert variable here*/ + /*insert API key here*/;
      
-        //Make the user input available to view and edit while results are visible. User input will probably also need to be compressible.
-        if($("map").show()){
-        	$("#user-input").show();
-        	$("#user-input").css({float:"top"});
-        }else {
-        	$("#user-input").hide();
+        /*!!Ignore the following step. We are hiding the search input after user searches!!*/   	//Make the user input available to view and edit while results are visible. User input will probably also need to be compressible.
+        
 
-        };//Now how do we expand and contract our user-input form while the map is on display?
-        var editInput = function(){
+        //if($("map").show()){
+        	//$("#user-input").css({"float":"top", "height":"0.5em", "width":"0.5em"});
+       // };//Now how do we expand and contract our user-input form while the map is on display?
+        
+        /*var editInput = function(){
         	var editMessage = $("h3").html("You can edit your filters by clicking here!");
         	var editExpand = $("button");
-        	$("editExpand").create(editMessage);
-        	$("editExpand").css({float:right});
-        	$("editExpand").click(function{
-        		$("#user-input").css({height:0.5em; width: 0.5em; float:right});
-        		});
-        };
+        	$("editExpandCommand").create(editMessage);
+        	$("editExpandCommand").css({float:right});
+        	var editExpandAccomplished = $("editExpandCommand").click(function(){
+        		$("#user-input").css({"height":"0.5em", "width":"0.5em", "float":"right"});//.css() methods do not follow CSS stylesheet rules. No semicolons, and always quote your CSS elements and properties.
+        		    });
+        	var editContract = function(){
+        		if $("editExpandAccomplished"){
+
+        		};
+        	};
+        };*/
+
         
 
 	 	 });//End document.ready
